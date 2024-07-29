5ed1d0918387cd9e1ef82a458802b67edb1f2e36
simondev413 <devemail413@gmail.com>
2024-07-29 16:39:07 +0000

    feat: created main excepetions classes.

--- spidertype/exceptions.py
+++ spidertype/exceptions.py
@@ -1,3 +1,32 @@
-class TypeValidationError(Exception):
-    def __init__(self,message) -> None:
+class SpiderTypeError(Exception):
+    def __init__(self,message):
+        super().__init__(message)
+
+class TypeValidationError(SpiderTypeError):
+    """
+    Excepetion raised when a type validation fail.
+    """
+    def __init__(self,message):
+        super().__init__(message)
+
+class RegistrationError(SpiderTypeError):
+    """
+    Exception raised when occur an error during type registation.
+    """
+    def __init__(self, message):
+        super().__init__(message)
+
+class TypeNotFoundError(SpiderTypeError):
+    """
+    Exception raised when a specific type is not found
+    """
+    def __init__(self, message):
+        super().__init__(message)
+    
+
+class InvalidTypeError(SpiderTypeError):
+    """
+    Exception raised when an invalid type is used.
+    """
+    def __init__(self, message):
         super().__init__(message)
